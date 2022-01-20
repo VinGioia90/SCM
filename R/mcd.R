1cba43fb8e659b0dc93ccc8daf5b266c261c8bee
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-01-20 11:28:21 +0100

    Modified Cholesky Decomposition Precision Matrix

--- R/mcd.R
+++ R/mcd.R
@@ -1,9 +1,9 @@
 #' Modified Cholesky decomposition
 #'
-#' @description The mcd function allows to obtain the unconstrained elements of the modified Cholesky decomposition
+#' @description The mcd function allows to obtain the unconstrained elements of the modified Cholesky decomposition of the precision matrix
 #' @param X A covariance matrix
 #'
-#' @return A matrix the unconstrained elements of the modified Cholesky decomposition...
+#' @return A matrix with the unconstrained elements of the modified Cholesky decomposition of the precision matrix
 #' @export
 #'
 #' @examples
