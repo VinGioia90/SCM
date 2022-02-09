b66d7083f1a46f99014c605bddf5e541fb41dcba
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-02-09 08:43:24 +0100

    Header file

---
+++ src/scm.h
@@ -0,0 +1,15 @@
+#ifndef _SCM_H
+#define _SCM_H
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+NumericMatrix lt_inversion(NumericMatrix& X);
+
+NumericMatrix mcd_LD(NumericVector& x, uint32_t& d);
+
+NumericMatrix mcd_Sigma(NumericMatrix& L, NumericMatrix& D,  uint32_t& d);
+
+
+
+#endif
