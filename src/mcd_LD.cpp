b4609be2ec371ef55d483ea2d0e68abf89bf520b
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-02-09 08:41:44 +0100

    LD elements (via cpp)

---
+++ src/mcd_LD.cpp
@@ -0,0 +1,27 @@
+#include<Rcpp.h>
+using namespace Rcpp;
+#include "scm.h"
+
+// [[Rcpp::export(name="mcd_LD")]]
+NumericMatrix mcd_LD(NumericVector& x, uint32_t& d){
+ NumericMatrix T(d,d);
+ NumericMatrix Y(d,d);
+ uint32_t j;
+ uint32_t k;
+ uint32_t count = d;
+ T(0,0) = 1.0;
+ for(j = 1; j < d; j++){
+  T(j,j) = 1.0;
+   for(k = 0; k < j; k++){
+    T(j,k) = x(count + d);
+    count += 1;
+   }
+  }
+
+ Y = lt_inversion(T);
+ for(j = 0; j < d; j++){
+  Y(j,j) = exp(0.5 * x[j + d]);
+ }
+
+ return(Y);
+}
