3ceeba25277910fba3531b46db66ca7768b8ff84
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-02-15 16:38:36 +0100

    mcd function

--- R/mcd.R
+++ R/mcd.R
@@ -1,4 +1,4 @@
-#' Modified Cholesky decomposition
+#' Modified Cholesky decomposition 2
 #'
 #' @description The mcd function allows to obtain the unconstrained elements of the modified Cholesky decomposition of the precision matrix
 #' @param X A covariance matrix
@@ -9,20 +9,7 @@
 #' @examples
 
 mcd <- function(X){
-  d <- ncol(X)
-  res <- matrix(0, d, d)
-  chol_dec <- chol(X)
-  D <- diag(chol_dec)
-  L <- matrix(0, d, d)
-  diag(L) <- 1
-  for(j in 2:d){
-    for(k in 1:(j - 1)){
-      L[j, k] <- chol_dec[k, j]/D[k]
-    }
-  }
-
-  res <- lowertri_inv(L)
-  res <- res + t(res)
-  diag(res) <- log(D^2)
-  return(res)
+ res <- mcd_decomposition(X)
+ return(res)
 }
+
