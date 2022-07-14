67542d969c520234fef9611ae14e4e3eb0041151
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-07-14 14:52:20 +0100

    update score

---
+++ src/scm_der2.h
@@ -0,0 +1,13 @@
+#ifndef _SCM_H
+#define _SCM_H
+
+
+
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+double d1_mcd_eta(const arma::mat& eta, const arma::mat& y, arma::mat& res, arma::vec& z, arma::vec& w, arma::mat& G);
+
+double d2_mcd_eta(const arma::mat& eta, const arma::mat& y, arma::mat& res,  Rcpp::List&  idx_jk, arma::vec& z, arma::vec& w, arma::mat& G, arma::vec& t);
+
+#endif
