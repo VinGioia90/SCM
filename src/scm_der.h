26ee635f6decddea195ae3d13f0e04cc2bc06f86
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:23:17 +0100

    Header derivatives

--- src/scm_der.h
+++ src/scm_der.h
@@ -6,8 +6,22 @@
 #include <RcppArmadillo.h>
 using namespace Rcpp;
 
-double d1_mcd_eta(NumericVector& eta, NumericVector& y, NumericVector& res, IntegerVector& z, IntegerVector& w, IntegerMatrix& G);
+// mcd parametrisation
+double d1_mcd_eta(const arma::mat& eta, const arma::mat& y, arma::mat& d1l, arma::vec& z, arma::vec& w, arma::mat& G);
+double d2_mcd_eta(const arma::mat& eta, const arma::mat& y, arma::mat& d2l, arma::vec& d2l_v,
+                  arma::vec& z, arma::vec& w, arma::mat& G, arma::vec& t,
+                  Rcpp::List&  b1_eta, Rcpp::List&  b3, arma::vec& ib1_eta, arma::vec& ib3);
+double d3_mcd_eta (const arma::mat& eta, const arma::mat& y, arma::mat& res,
+                   arma::vec& z, arma::vec& w, arma::mat& G, arma::vec& t,
+                   Rcpp::IntegerVector& h, Rcpp::IntegerVector& h2, Rcpp::IntegerVector& h3,
+                   Rcpp::List& idx3_1, Rcpp::List& idx3_2,  Rcpp::List& idx3_3,
+                   Rcpp::List& idx3_4, Rcpp::List& idx3_5, Rcpp::List& idx3_6, Rcpp::List& idx_neq0);
 
-double d2_mcd_eta(const arma::mat& eta, const arma::mat& y, arma::mat& res,  Rcpp::List&  idx_jk, arma::vec& z, arma::vec& w, arma::mat& G, arma::vec& t);
+// logm parametrisation
+double  d1_logm_eta(const arma::mat& eta, const arma::mat& y, arma::mat& d1l, arma::vec& z, arma::vec& w);
+double  d2_logm_eta(const arma::mat& eta, const arma::mat& y, arma::mat& d2l, arma::vec& d2l_v,
+                    arma::vec& z, arma::vec& w, Rcpp::List&  b1_eta, Rcpp::List&  b3, arma::vec& ib1_eta, arma::vec& ib3);
+
+
 
 #endif
