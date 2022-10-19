5f0665fa11a9e6c08b8984eab306bf6628183f05
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:24:45 +0100

    PRecision matrix

--- src/precision.cpp
+++ src/precision.cpp
@@ -3,9 +3,8 @@
 
 // [[Rcpp::export(name="precision")]]
 Rcpp::NumericMatrix precision(Rcpp::NumericMatrix& X){
- using namespace Rcpp;
- arma::mat res;
- res = arma::inv_sympd(as<arma::mat>(X));
-
- return(wrap(res));
+  using namespace Rcpp;
+  arma::mat res;
+  res = arma::inv_sympd(as<arma::mat>(X));
+  return(wrap(res));
 }
