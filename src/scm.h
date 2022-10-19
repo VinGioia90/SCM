78c25317e0522a23d311c596e34e4ed44edd3ba2
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:23:37 +0100

    Header useful functions

--- src/scm.h
+++ src/scm.h
@@ -3,12 +3,14 @@
 
 #include <RcppArmadillo.h>
 using namespace Rcpp;
-NumericMatrix lt_inversion(NumericMatrix& X);
+//NumericMatrix lt_inversion(NumericMatrix& X);
+arma::mat lt_inversion(arma::mat& X);
 
-NumericMatrix mcd_LD(NumericVector& x, uint32_t& d);
+//NumericMatrix mcd_LD(NumericVector& x, uint32_t& d);
+arma::mat mcd_LD(arma::rowvec& x, uint32_t& d);
 
-NumericMatrix mcd_Sigma(NumericMatrix& L, NumericMatrix& D,  uint32_t& d);
+//NumericMatrix mcd_Sigma(NumericMatrix& L, NumericMatrix& D,  uint32_t& d);
+arma::mat mcd_Sigma(arma::mat& L, arma::mat& D,  uint32_t& d);
 
-NumericMatrix logM_Sigma(NumericVector& x,   uint32_t& d);
-
+arma::mat logM_Sigma(arma::rowvec& x,   uint32_t& d);
 #endif
