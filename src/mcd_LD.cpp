737efe8b4d967d7cc40d41dd6715a2fc2d0a8bc7
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:28:56 +0100

    Computing L and D of the mcd parametrisation

--- src/mcd_LD.cpp
+++ src/mcd_LD.cpp
@@ -1,27 +1,29 @@
 #include "scm.h"
 
 // [[Rcpp::export(name="mcd_LD")]]
-Rcpp::NumericMatrix mcd_LD(Rcpp::NumericVector& x, uint32_t& d){
- using namespace Rcpp;
+arma::mat mcd_LD(arma::rowvec& x, uint32_t& d){
+  using namespace arma;
 
- NumericMatrix T(d,d);
- NumericMatrix Y(d,d);
- uint32_t j;
- uint32_t k;
- uint32_t count = d;
- T(0,0) = 1.0;
- for(j = 1; j < d; j++){
-  T(j,j) = 1.0;
-   for(k = 0; k < j; k++){
-    T(j,k) = x(count + d);
-    count += 1;
-   }
+  //NumericMatrix T(d, d);
+  //NumericMatrix Y(d, d);
+  mat T(d, d, fill::zeros);
+  mat Y(d, d, fill::zeros);
+
+  uint32_t j;
+  uint32_t k;
+  uint32_t count = d;
+  T(0,0) = 1.0;
+  for(j = 1; j < d; j++){
+    T(j, j) = 1.0;
+    for(k = 0; k < j; k++){
+      T(j, k) = x(count + d);
+      count += 1;
+    }
   }
 
- Y = lt_inversion(T);
- for(j = 0; j < d; j++){
-  Y(j,j) = exp(0.5 * x[j + d]);
- }
-
- return(Y);
+  Y = lt_inversion(T);
+  for(j = 0; j < d; j++){
+    Y(j, j) = exp(0.5 * x(j + d));
+   }
+  return(Y);
 }
