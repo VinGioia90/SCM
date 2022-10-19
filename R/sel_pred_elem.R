affd66bb9ac80383be50b4ba433dd4a3118afd65
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:45:03 +0100

    Select the elements to predict

--- R/sel_pred_elem.R
+++ R/sel_pred_elem.R
@@ -1,28 +1,17 @@
-#' Prediction Element Index
-#'
-#' @description Select the prediction element
-#' @param d The dimension of the outcome
-#'
-#' @return The index selected
-#' @export
-#'
-#' @examples
-# idx <- sel_elem(4)
-# idx(3,1)
 sel_elem <- function(d){
   pred_elem <- function(j,k){
 
     CovCor_mat <- function(d){
-      G <- matrix(0, d, d)
-      diag(G) <- 1:d
+      Cm <- matrix(0, d, d)
+      diag(Cm) <- 1:d
       count <- d + 1
       for(i in 2:d){
         for(j in 1:(i - 1)){
-          G[i,j] <- G[j,i] <- count
+          Cm[i,j] <- Cm[j,i] <- count
           count <- count + 1
         }
       }
-      return(G+d)
+      return(Cm+d)
     }
 
     return(CovCor_mat(d)[j,k])
