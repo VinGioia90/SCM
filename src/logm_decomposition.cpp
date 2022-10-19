c27a98b93c2e6b0835e73e9e8c0bdae6142d2337
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:27:26 +0100

    logM decomposition

--- src/logm_decomposition.cpp
+++ src/logm_decomposition.cpp
@@ -2,17 +2,15 @@
 #include <RcppArmadillo.h>
 
 // [[Rcpp::export(name="logm_decomposition")]]
-Rcpp::NumericMatrix logm_decomposition(Rcpp::NumericMatrix& X){
- using namespace Rcpp;
- 
- arma::mat eigvec;
- arma::vec eigval;
+arma::mat logm_decomposition(arma::mat& X){
+ using namespace arma;
 
- arma::mat res;
- 
- arma::eig_sym(eigval,eigvec,as<arma::mat>(X));
- 
- res = eigvec * diagmat(log(eigval))* eigvec.t(); 
+ mat eigvec;
+ vec eigval;
+ mat out;
 
- return(wrap(res));
+ eig_sym(eigval, eigvec, X);
+ out = eigvec * diagmat(log(eigval)) * eigvec.t();
+
+ return(out);
 }
