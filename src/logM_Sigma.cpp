02aff5b82620e3c14fe0a4cd0f1c42907b94ed28
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:28:32 +0100

    Sigma from logSigma

--- src/logM_Sigma.cpp
+++ src/logM_Sigma.cpp
@@ -2,31 +2,25 @@
 #include <RcppArmadillo.h>
 
 // [[Rcpp::export(name="logM_Sigma")]]
-Rcpp::NumericMatrix logM_Sigma(Rcpp::NumericVector& x, uint32_t& d){
- using namespace Rcpp;
+arma::mat logM_Sigma(arma::rowvec& x, uint32_t& d){
+ using namespace arma;
 
- arma::mat logSigma(d, d, arma::fill::zeros);
- arma::mat Sigma(d, d, arma::fill::zeros);
+ mat logSigma(d, d, arma::fill::zeros);
+ mat Sigma(d, d, arma::fill::zeros);
 
  uint32_t j;
  uint32_t k;
 
- arma::vec x2 = as<arma::vec>(x);
-
-
- logSigma(0,0) = x2(d);
+ logSigma(0, 0) = x(d);
  uint32_t count = d;
-
  for(j = 1; j < d; j++){
-  logSigma(j,j) = x2(j+d);
+  logSigma(j, j) = x(j + d);
   for(k = 0; k <  j; k++){
-   logSigma(j,k) = logSigma(k,j) = x2(count + d);
+   logSigma(j, k) = logSigma(k, j) = x(count + d);
    count += 1;
   }
  }
 
- Sigma = arma::expmat_sym(logSigma);
-
-
- return(wrap(Sigma));
+ Sigma = expmat_sym(logSigma);
+ return(Sigma);
 }
