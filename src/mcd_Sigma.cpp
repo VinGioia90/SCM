624f8e902dd5cda3520f5243699f99a80d4ab094
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:29:12 +0100

    Sigma from the mcd decomposition

--- src/mcd_Sigma.cpp
+++ src/mcd_Sigma.cpp
@@ -1,24 +1,26 @@
-#include <Rcpp.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+
 // [[Rcpp::export(name="mcd_Sigma")]]
-Rcpp::NumericMatrix mcd_Sigma(Rcpp::NumericMatrix& L, Rcpp::NumericMatrix& D, uint32_t& d){
- using namespace Rcpp;
+arma::mat mcd_Sigma(arma::mat& L, arma::mat& D, uint32_t& d){
+  using namespace arma;
 
- NumericMatrix P(d,d);
- NumericMatrix Sigma(d,d);
+  mat P(d, d);
+  mat Sigma(d, d);
 
- uint32_t j;
- uint32_t k;
- uint32_t l;
+  uint32_t j;
+  uint32_t k;
+  uint32_t l;
 
- for(j = 0; j < d; j++){
-  P(j,j) = D(j,j);
-  if(j > 0){
-   for(k = 0; k < j; k++){
-     P(j, k) = D(k, k) * L(j, k);
-   }
+  for(j = 0; j < d; j++){
+    P(j, j) = D(j, j);
+    if ( j > 0 ) {
+      for(k = 0; k < j; k++){
+        P(j, k) = D(k, k) * L(j, k);
+      }
+    }
   }
- }  
-  
+
   for(j = 0; j < d; j++){
     for(k = 0; k < (j + 1); k++){
       for(l = 0; l < (j + 1); l++){
