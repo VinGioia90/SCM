de4ddfa01963361ef1d4806d139373ab6661337d
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:32:19 +0100

    Package Information

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,16 @@
-#' mypackage: A package for computating the notorious bar statistic.
+#' SCM: A package for multivariate gaussian additive models with smoothing covariance modelling
 #'
-#' The mypackage package provides three categories of important functions:
-#' foo, bar and baz.
+#' The SCM package provides some important functions to fit the model and to see the model output
 #'
 #' @section Mypackage functions:
-#' The mypackage functions ...
+#' gam_scm
+#' mvn_scm
+#' internal
+#' summary.scm
+#' mcd
+#' logm
+#' precision
+#' plot_EPCD
 #'
 #' @docType package
 #' @name SCM
