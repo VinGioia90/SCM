f41e9e5dfa209c6fde5b24cb64a652122f9a362b
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-19 10:28:08 +0100

    Lower triangular L matrix inversion

--- src/lt_inversion.cpp
+++ src/lt_inversion.cpp
@@ -1,27 +1,28 @@
-#include <Rcpp.h>
-// [[Rcpp::export(name="lt_inversion")]]
-Rcpp::NumericMatrix lt_inversion(Rcpp::NumericMatrix& X){
- using namespace Rcpp;
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
 
- uint32_t d = X.cols();
+// [[Rcpp::export(name="lt_inversion")]]
+arma::mat lt_inversion(arma::mat& X){
+ using namespace arma;
+
+ uint32_t d = X.n_cols;//();
  uint32_t j;
  uint32_t k;
  uint32_t l;
  double s;
- NumericMatrix res(d,d);
+ mat out(d, d, fill::zeros);
 
  for(j = 0; j < d; j++){
-  res(j,j) = 1.0;
+  out(j, j) = 1.0;
   if(j > 0){
    for(k = 0; k < j; k++){
     s = 0.0;
     for(l = k; l < j; l++){
-     s += X(j,l) * res(l,k);
+     s += X(j, l) * out(l, k);
     }
-    res(j,k) = -s;
+    out(j, k) = -s;
    }
-  } 
+  }
  }
-
- return(res);
+ return(out);
 }
