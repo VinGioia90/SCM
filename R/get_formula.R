6a0a8e2d7170761f8fff49e69ee595d8857a2c99
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-20 14:42:54 +0100

    get_formula function

--- R/get_formula.R
+++ R/get_formula.R
@@ -1,4 +1,3 @@
-
 #' This function take the list of formulas provide by the user, then processing it  to be passed to the gam function to estimate gam_scm model
 #'
 #' @description These is a function that fits a GAM model via covariance modelling using [mgcv::gam]
@@ -9,17 +8,59 @@
 #' @export
 #'
 #' @importFrom stringr word
+#' @importFrom BMisc rhs lhs.vars
 #' @examples
 
 get_foo <- function ( foo_user, d) {
-  #d <-4 # come beccare d????
   d_len <- nchar(d)
   foo_len <- length(foo_user)
+  foo_bar_lhs <- list()
+  foo_bar <-list()
+  el_bar <- rep(0, length(foo_user))
+  el_bar2 <- rep(0, length(foo_user))
+  count<-1
+  for(j in 1:length(foo_user)){
+    idx_bar <- gregexpr("\\|", foo_user[[j]])[[2]]
+    if(idx_bar[1] != -1){
+      el_bar[j] <- j
+      idx_bar <- c(0,idx_bar,(nchar( word(as.character(foo_user[[j]]), 1, sep = "\\~")[2]))+1 )
+      for(i in 1:(length(idx_bar)-1)){
+        foo_bar_lhs[[count]] <- substring(as.character(foo_user[[j]])[2], first = idx_bar[i]+1, last = idx_bar[i + 1] -1)
+        foo_bar[[count]] <- paste(foo_bar_lhs[[count]], "~", as.character(rhs(foo_user[[j]]))[2])
+        count <- count + 1
+      }
+    }
+  }
+
+
+  for(j in (1:length(foo_user))[-el_bar]){
+    for(k in 1 : length(foo_bar)){
+      if(lhs.vars(foo_user[[j]]) ==  lhs.vars(formula(foo_bar[[k]]))){
+        foo_bar[[k]] <- paste(foo_bar[[k]],as.character( rhs(foo_user[[j]]))[2], sep="+")
+        el_bar2[j] <- j
+      }
+    }
+  }
+  name_lhs <- rep(0, length(foo_bar))
+  for(j in 1:length(foo_bar)){
+    name_lhs[j]<-gsub("[[:space:]]", "",  word(as.character(foo_bar[[j]]), 1, sep = "\\~"))
+  }
+
+  count <- length(foo_bar) + 1
+  for(j in (1:length(foo_user))[-c(el_bar,el_bar2)]){
+    if( !(as.character(lhs.vars(foo_user[[j]])) %in%   name_lhs)) foo_bar[[count]] <- foo_user[[j]]
+    count <- count + 1
+  }
+
+
+  foo_user2 <- lapply(foo_bar, formula, env = globalenv())
+
+  foo_len <- length(foo_user2)
 
   if ( foo_len > (d + d*(d + 1)/2) ) stop("More formulas than elements to be modelled")
   #library(stringr)
   # Check on the mean model formula
-  lhs <- unlist(lapply(1:foo_len, function(x) word(as.character(foo_user[[x]]), 1, sep = "\\~")[2]))
+  lhs <- unlist(lapply(1:foo_len, function(x) word(as.character(foo_user2[[x]]), 1, sep = "\\~")[2]))
   meanlhs <- lhs[which(!word(lhs, 1, sep = "\\Th_")=="")]
   if ( length(meanlhs) != d ) stop("More/Less mean components than d")
   if ( length(meanlhs) > length(unique(meanlhs)) ) stop("A mean component is modelled more than one time")
@@ -33,16 +74,16 @@ get_foo <- function ( foo_user, d) {
   nrc[[2]] <- list()
   nrc[[3]] <- list()
   for( j in (d + 1) : foo_len ) {
-    if( nchar(word(as.character(foo_user[[j]]), 1, sep = "\\~")[2]) == (3 + 2 * d_len) ) {
-      nrc[[1]][j - d, 1] <- as.integer(substring(format(foo_user[[j]]), first = 4, last = 4 + d_len - 1))
+    if( nchar(word(as.character(foo_user2[[j]]), 1, sep = "\\~")[2]) == (3 + 2 * d_len) ) {
+      nrc[[1]][j - d, 1] <- as.integer(substring(format(foo_user2[[j]]), first = 4, last = 4 + d_len - 1))
       if( nrc[[1]][j - d, 1] == 0 | nrc[[1]][j - d, 1] > d) stop("Wrong specification of the formula: the elements 0 and the lements greater than d  could not be specified")
-      nrc[[1]][j - d, 2] <- as.integer(substring(format(foo_user[[j]]), first = 4 + d_len, last = 4 + 2 * d_len - 1))
+      nrc[[1]][j - d, 2] <- as.integer(substring(format(foo_user2[[j]]), first = 4 + d_len, last = 4 + 2 * d_len - 1))
       if( nrc[[1]][j - d, 2] == 0 | nrc[[1]][j - d, 2] > d) stop("Wrong specification of the formula: the elements 0 and the lements greater than d  could not be specified")
       if ( nrc[[1]][j - d, 1] >  nrc[[1]][j - d, 2] ) {
         nrc[[1]][j - d,] <- nrc[[1]][j - d, c(2:1)]
       }
-      nrc[[2]][[j - d]] <- substring(format(foo_user[[j]]), first = 4 + 2 * d_len)
-      nrc[[3]][[j - d]] <- format(foo_user[[j]])
+      nrc[[2]][[j - d]] <- substring(format(foo_user2[[j]]), first = 4 + 2 * d_len)
+      nrc[[3]][[j - d]] <- format(foo_user2[[j]])
     } else {
       stop("Wrong formula initialization, if the dimension of the outcome is < 10 specify two digits after Th_,
                                           if it is > 9 and < 100 specify four digits after Th_,
@@ -53,7 +94,7 @@ get_foo <- function ( foo_user, d) {
   if ( nrow(unique(nrc[[1]][,1:2])) <  ( foo_len -d) ) stop ("Two elements are associated to different formulas")
 
   # Facciamno un controllo sulla specificazione del mean model
-  mean_foo <- lapply(1:d, function(x) foo_user[[x]])  # the first d components of the list lies on mean model specification
+  mean_foo <- lapply(1:d, function(x) foo_user2[[x]])  # the first d components of the list lies on mean model specification
   cov_foo <- internal()$formula_init(d) # initialization of the covariance model
   cov_foop <- list() # initialization of the covariance model (to be printed)
   cov_foos <- list() # initialization of the covariance model (auxiliary)
@@ -87,5 +128,8 @@ get_foo <- function ( foo_user, d) {
   }
 
   return (list(foo_eval = c(mean_foo, cov_foo), foo_print = c(mean_foo,cov_foop), foo_summary=c(mean_foo, cov_foos)))
+
 }
+
+
 
