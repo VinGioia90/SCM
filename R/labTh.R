cd162a1c35ed3f8ccc3ac2436f1f6716356de6ce
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-26 14:00:40 +0100

    Label Theta function

--- R/labTh.R
+++ R/labTh.R
@@ -1,16 +1,16 @@
 labTh <- function(d, k){
-  d<-4
+  # This function  creates the labels used in the modified print summary function
   Thmat <- matrix(NA, d, d)
-  for(i in 1:d){
-    Thmat[i,i] <- paste0("Th_",i,i)
-    if(i >1){
+  for ( i in 1 : d ) {
+    Thmat[i, i] <- paste0("Th_", i, i)
+    if ( i > 1 ) {
       for(j in 1:(i-1)){
-        Thmat[j,i] <- paste0("Th_",j,i)
+        Thmat[j,i] <- paste0("Th_", j, i)
       }
     }
   }
-  Thvec <- c(diag(Thmat),Thmat[upper.tri(Thmat, diag=FALSE)])
-  return(Thvec[k-d])
+  Thvec <- c(diag(Thmat), Thmat[upper.tri(Thmat, diag = FALSE)])
+  return(Thvec[k - d])
 }
 
 
