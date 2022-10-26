c9a8f95a2ffad9f18cc36dc5b6b0b644b62a33f3
Vincenzo Gioia <gioiavincenzo.25790@gmail.com>
2022-10-26 13:58:58 +0100

    Note: Things to do

--- R/Sigma.R
+++ R/Sigma.R
@@ -1,4 +1,8 @@
 Sigma_mat <- function(pred_Sigma){
+  #!!!! rendere visibile???? Si costruire una funzione che prenda in input la matrice degli lpi e restituisce una lista
+  # Such function takes the i-th predicted linear predictor element of the covariance modelling
+  #(so excluding the mean vector and when predict is used in combination with type="response")
+  # and return the covariance/correlation matrix (according to the flag)
   n_el <- length(pred_Sigma)
   d <- (-1 + sqrt(1 + 8 * n_el))/2
   res <- matrix(0, d, d)
